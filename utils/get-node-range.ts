4d94d76921c74c0cedfcfcc544f24712ce208729
Brad Zacher <brad.zacher@gmail.com>
2026-01-07 16:47:32 +1030

    Update import for TSESLint from utils

--- utils/get-node-range.ts
+++ utils/get-node-range.ts
@@ -1,5 +1,5 @@
 import type { TSESTree } from '@typescript-eslint/types'
-import type { TSESLint } from '@typescript-eslint/utils'
+import type * as TSESLint from '@typescript-eslint/utils/ts-eslint';
 
 import * as ASTUtils from '@typescript-eslint/utils/ast-utils'
 
